e749daa33b5619560b6d21ec444b67d338861ab3
Ashiq <web3.0.ashiq@gmail.com>
2023-04-22 07:50:53 +0600

    Database connected

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import  express  from "express";
+import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import connectDB from "./config/db";
+
 
 dotenv.config();
+connectDB();
 const app = express();
+
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req,res)=>{
   res.send('Server is running...')
 });
 
-app.listen(5000,()=>console.log("Server is runnning port 5000"))
+app.listen(PORT,()=>console.log(`Server is runnning port ${PORT}`))
